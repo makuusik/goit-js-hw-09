11ac9c0a4770821f0acd8c988b3f3e0d18429659
makuusik <119566111+makuusik@users.noreply.github.com>
2023-07-08 23:50:15 +0000

    Deploying to gh-pages from @ makuusik/goit-js-hw-09@4a402db11d002817a1a25232b7d58d9b875de7c5 🚀

--- 01-color-switcher.67b98394.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=null;function n(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;document.body.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,o=setInterval(n,666)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o),document.body.style.backgroundColor=""}));
-//# sourceMappingURL=01-color-switcher.67b98394.js.map
--- 01-color-switcher.91d8c3c2.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function o(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(o,666)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n),document.body.style.backgroundColor=""}))}();
-//# sourceMappingURL=01-color-switcher.91d8c3c2.js.map
